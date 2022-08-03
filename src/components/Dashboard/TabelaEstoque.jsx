c6e2f8d59d9211aec4def875712b3705b5165e0f
Alexandre Schwartzman <31558026+ASchwartzman@users.noreply.github.com>
2022-08-03 10:42:56 -0300

    Remove button & Rows update

--- src/components/Dashboard/TabelaEstoque.jsx
+++ src/components/Dashboard/TabelaEstoque.jsx
@@ -13,15 +13,16 @@ import {
   Tooltip,
 } from '@chakra-ui/react'
 import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
+import { removeEstoqueItem } from '../../firebase/firestore'
 
 const TableHead = () => {
   return (
     <Thead>
       <Tr>
-        <Th>ID</Th>
         <Th>Produto</Th>
+        <Th textAlign='center'>Caixa</Th>
+        <Th isNumeric>{"Peso (kg)"}</Th>
         <Th isNumeric>Quantidade</Th>
-        <Th isNumeric>Caixa</Th>
         <Th isNumeric>Estoque Mín</Th>
         <Th></Th>
       </Tr>
@@ -30,17 +31,19 @@ const TableHead = () => {
 }
 
 const TableItem = ({ item }) => {
+
+
   return (
     <Tr
       fontSize='md'
       color={'gray.600'}
       _hover={{ backgroundColor: 'gray.50' }}>
-      <Td>{item.id}</Td>
       <Td>{item.produto}</Td>
+      <Td borderColor='gray.200' borderRadius='lg' fontWeight='extrabold' bg='gray.100' textAlign='center' >{item.caixa}</Td>
+      <Td isNumeric>{item.peso}</Td>
       <Td fontWeight='bold' isNumeric>
         {item.quantidade}
       </Td>
-      <Td isNumeric>{item.caixa}</Td>
       <Td isNumeric>{item.estoqueMin}</Td>
       <Td>
         <HStack>
@@ -48,7 +51,7 @@ const TableItem = ({ item }) => {
             <IconButton color='teal' variant='ghost' icon={<EditIcon />} />
           </Tooltip>
           <Tooltip label='Excluir'>
-            <IconButton color='red' variant='ghost' icon={<DeleteIcon />} />
+            <IconButton color='red' variant='ghost' icon={<DeleteIcon />} onClick={() => removeEstoqueItem(item.id)} />
           </Tooltip>
         </HStack>
       </Td>
@@ -57,6 +60,7 @@ const TableItem = ({ item }) => {
 }
 
 export function TabelaEstoque({ items, ...otherProps }) {
+
   return (
     <Table variant='simple' w='100%'>
       <TableCaption placement='top'>
