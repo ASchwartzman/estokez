058fdbea1b7b97ab05241c6e1032ae0479610d5e
ASchwartzman <aleschwartzman22@gmail.com>
2022-08-07 08:45:45 -0300

    Fixed children prop

--- src/components/Dashboard/SearchBar.jsx
+++ src/components/Dashboard/SearchBar.jsx
@@ -3,11 +3,9 @@ import { InputGroup, InputLeftElement, Input, Icon } from '@chakra-ui/react'
 export function SearchBar({ leftIcon, handleChange, ...otherProps }) {
   return (
     <InputGroup {...otherProps}>
-      <InputLeftElement
-        pointerEvents='none'
-        color='gray.600'
-        children={<Icon as={leftIcon} />}
-      />
+      <InputLeftElement pointerEvents='none' color='gray.600'>
+        <Icon as={leftIcon} />
+      </InputLeftElement>
       <Input
         placeholder='Busca por PRODUTO ou CAIXA'
         variant='filled'
